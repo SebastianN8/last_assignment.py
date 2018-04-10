3006786cfc6398a0765279439ce1477fd75d4f85
Sebastian_N8 <sbastian.navas@ocsbstudent.ca>
2018-04-09 23:23:04 -0400

    Initial commit

---
+++ last_assignment.py
@@ -0,0 +1,30 @@
+#
+# last_assignment.py
+#
+# Created by: Sebastian N
+# Created on: April 8
+#
+# This program does loops
+
+# Including time module
+import time;
+
+# My variables
+guess_time = 0
+local_time = time.localtime(time.time()) # This is where the exact time is stored
+actual_seconds = local_time[5] # This is where just the seconds are stored
+
+print actual_seconds
+
+while guess_time is not actual_seconds:
+	actual_seconds = local_time[5]
+	guess_time = input('What do you think are the seconds past since the last change of minute: ')
+	print actual_seconds
+	if guess_time < 0 or guess_time > 60:
+		print 'That is an invalid answer!'
+	elif guess_time == actual_seconds:
+		print 'You guessed the seconds!'
+	elif guess_time > actual_seconds:
+		print 'You might want to wait a bit to input that answer!'
+	elif guess_time < actual_seconds:
+		print 'You are behind the actual_time!'
